793ab61e691f6f1fb929bc42fc2a626aa3d30bf4
Dominik Bobak <bobakdominik164@gmail.com>
2025-05-10 19:14:14 +0200

    Adding zoom and pan. Opening application in full screen.

--- VRGHeightMap/HeightMapApp/Components/MapViewer.xaml.cs
+++ VRGHeightMap/HeightMapApp/Components/MapViewer.xaml.cs
@@ -11,6 +11,12 @@ namespace HeightMapApp.Components
     /// </summary>
     public partial class MapViewer : UserControl
     {
+        private readonly double _minScale = 0.1;
+        private readonly double _maxScale = 8.0;
+        private bool _isPanning = false;
+        private bool _captureCursorPosition = false;
+        private Point _panStartCursorPosition;
+        private Point _panOriginGridPosition;
         private MapViewerViewModel ViewModel => (MapViewerViewModel)DataContext;
 
         public MapViewer()
@@ -20,20 +26,30 @@ namespace HeightMapApp.Components
 
         private void ImageGrid_OnMouseEnter(object sender, MouseEventArgs e)
         {
-            var grid = (Grid)sender;
-            var normalizedCrsorPosition = NormalizeCursorPosition(e.GetPosition(this), grid);
-            ViewModel.UpdateCursorPosition(normalizedCrsorPosition.Y, normalizedCrsorPosition.X, grid.ActualHeight, grid.ActualWidth);
+            _captureCursorPosition = true;
         }
 
         private void ImageGrid_OnMouseMove(object sender, MouseEventArgs e)
         {
-            var grid = (Grid)sender;
-            var normalizedCursorPosition = NormalizeCursorPosition(e.GetPosition(this), grid);
-            ViewModel.UpdateCursorPosition(normalizedCursorPosition.Y, normalizedCursorPosition.X, grid.ActualHeight, grid.ActualWidth);
+            if (_isPanning)
+            {
+                var current = e.GetPosition(ImageScrollViewer);
+                var delta = current - _panStartCursorPosition;
+
+                ImageGridTranslateTransform.X = _panOriginGridPosition.X + delta.X;
+                ImageGridTranslateTransform.Y = _panOriginGridPosition.Y + delta.Y;
+            }
+
+            if (_captureCursorPosition)
+            {
+                var normalizedCursorPosition = NormalizeCursorPosition(e);
+                ViewModel.UpdateCursorPosition(normalizedCursorPosition.Y, normalizedCursorPosition.X, ImageGrid.ActualHeight, ImageGrid.ActualWidth);
+            }            
         }
 
         private void ImageGrid_OnMouseLeave(object sender, MouseEventArgs e)
         {
+            _captureCursorPosition = false;
             ViewModel.ResetCursorPosition();
         }
         private void ImageGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -50,27 +66,63 @@ namespace HeightMapApp.Components
             };
         }
 
-        private Point NormalizeCursorPosition(Point cursorPozition, Grid grid)
+        private Point NormalizeCursorPosition(MouseEventArgs e)
         {
-            var imageHeight = grid.ActualHeight;
-            var imageWidth = grid.ActualWidth;
-            var imageStartPoint = GetImageStartingPoint(grid);
+            var cursorPosition = e.GetPosition(ImageGrid);
+            var normalizedX = cursorPosition.X / ImageGrid.ActualWidth;
+            var normalizedY = cursorPosition.Y / ImageGrid.ActualHeight;
+            
+            normalizedX = Math.Clamp(normalizedX, 0, 1);
+            normalizedY = Math.Clamp(normalizedY, 0, 1);
 
-            if (imageHeight == 0 || imageWidth == 0)
+            // Sometimes the cursor position is 1 (mostly right bottom corner during zoom),
+            // which causes problems with the calculations
+            if (normalizedX == 1 || normalizedY == 1)
             {
                 return new Point(0, 0);
             }
-            else
+            return new Point(normalizedX, normalizedY);
+        }
+
+        private void ImageScrollViewer_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if ((e.Delta > 0 && ImageGridScaleTransform.ScaleX < _maxScale && ImageGridScaleTransform.ScaleY < _maxScale) ||
+                (e.Delta < 0 && ImageGridScaleTransform.ScaleX > _minScale && ImageGridScaleTransform.ScaleY > _minScale))
             {
-                var normalizedX = ((cursorPozition.X - imageStartPoint.X)/ imageWidth);
-                var normalizedY = ((cursorPozition.Y - imageStartPoint.Y)/ imageHeight);
-                return new Point(normalizedX, normalizedY);
+                var zoom = e.Delta > 0 ? 1.1 : 0.9;
+                var cursorPosition = e.GetPosition(ImageGrid);
+
+                ImageGridScaleTransform.ScaleX *= zoom;
+                ImageGridScaleTransform.ScaleY *= zoom;
+
+                ImageGridScaleTransform.CenterX *= zoom;
+                ImageGridScaleTransform.CenterY *= zoom;
+
+                ImageGridTranslateTransform.X = (1 - zoom) * cursorPosition.X + zoom * ImageGridTranslateTransform.X;
+                ImageGridTranslateTransform.Y = (1 - zoom) * cursorPosition.Y + zoom * ImageGridTranslateTransform.Y;
             }
         }
 
-        private Point GetImageStartingPoint(Grid grid)
+        private void ImageScrollViewer_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-            return grid.TranslatePoint(new Point(0, 0), this);
+            _isPanning = true;
+            _panStartCursorPosition = e.GetPosition(this);
+            _panOriginGridPosition = new Point(ImageGridTranslateTransform.X, ImageGridTranslateTransform.Y);
+            Cursor = Cursors.Hand;
+            ImageScrollViewer.CaptureMouse();
+        }
+
+        private void ImageScrollViewer_OnMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isPanning = false;
+            Cursor = Cursors.Arrow;
+            ImageScrollViewer.ReleaseMouseCapture();
+        }
+
+        private void ImageScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ImageScrollViewer_OnMouseWheel( sender, e);
+            e.Handled = true;
         }
     }
 }
